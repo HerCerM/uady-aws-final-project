dd81dab6667072d9bf244a86283676d25151f9a7
Hernán Cervera <hjcerveram@hotmail.com>
2022-04-03 19:48:12 -0500

    Initial project setup

---
+++ src/main/java/hercerm/college/uadyawsfinalproject/UadyAwsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package hercerm.college.uadyawsfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UadyAwsFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UadyAwsFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hercerm/college/uadyawsfinalproject/UadyAwsFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hercerm.college.uadyawsfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UadyAwsFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
